c261e9ea37ca7292f40ed1b96f3be39dde08cfad
AHY.8507 <101252560+AHY8507@users.noreply.github.com>
2023-04-10 17:52:53 +0330

    Create Game.py

---
+++ Game.py
@@ -0,0 +1,42 @@
+import pygame
+
+pygame.init()
+
+def load_playground():
+    global win , win_length , win_width
+    win_length = 1200
+    win_width = 800
+    win = pygame.display.set_mode((win_length , win_width))
+    pygame.display.set_caption("Game")
+
+    pygame.draw.rect(win , (0 , 255 , 255) , ((50 , 50) , (win_length - 100 , win_width - 100)) , 3)
+    pygame.draw.rect(win , (0 , 255 , 255) , ((50 , (win_width / 2) - 75) , (50 , 150)) , 3)
+    pygame.draw.rect(win , (0 , 255 , 255) , ((win_length - 100 , (win_width / 2) - 75) , (50 , 150)) , 3)
+    
+    pygame.draw.circle(win , (255 , 255 , 255) , (win_length / 2 , win_width / 2) , 20)
+
+def load_left_side():
+    pygame.draw.circle(win , (255 , 0 , 255) , (250 , 100 + 20) , 30 , 5)
+    pygame.draw.circle(win , (255 , 0 , 255) , (200 , win_width / 2) , 30 , 5)
+    pygame.draw.circle(win , (255 , 0 , 255) , (250 , (win_width - 100) - 20) , 30 , 5)
+
+def load_right_side():
+    pygame.draw.circle(win , (0 , 255 , 0) , (win_length - 250 , 100 + 20) , 30 , 5)
+    pygame.draw.circle(win , (0 , 255 , 0) , (win_length - 200 , win_width / 2) , 30 , 5)
+    pygame.draw.circle(win , (0 , 255 , 0) , (win_length - 250 , (win_width - 100) - 20) , 30 , 5)
+
+def running():
+    load_playground()
+    load_left_side()
+    load_right_side()
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    running()
