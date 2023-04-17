bcfee2f348afde736cc49877816e7da5c53fd313
AHY.8507 <101252560+AHY8507@users.noreply.github.com>
2023-04-18 03:03:42 +0430

    Update Game.py
    First step of animation

--- Game.py
+++ Game.py
@@ -13,6 +13,8 @@ def load_playground():
     pygame.draw.rect(win , (0 , 255 , 255) , ((50 , (win_width / 2) - 75) , (50 , 150)) , 3)
     pygame.draw.rect(win , (0 , 255 , 255) , ((win_length - 100 , (win_width / 2) - 75) , (50 , 150)) , 3)
     
+
+def load_ball():
     pygame.draw.circle(win , (255 , 255 , 255) , (win_length / 2 , win_width / 2) , 20)
 
 def load_left_side():
@@ -26,11 +28,11 @@ def load_right_side():
     pygame.draw.circle(win , (0 , 255 , 0) , (win_length - 250 , (win_width - 100) - 20) , 30 , 5)
 
 def running():
-    load_playground()
-    load_left_side()
-    load_right_side()
-    
     while True:
+        load_playground()
+        load_ball()
+        load_left_side()
+        load_right_side()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
